c03d422a4485dfdbc717ddc630c561bf704028f0
CoRine65 <coderine65@gmail.com>
2025-08-12 23:55:27 -0700

    Dynamically added some html content vis JS

--- README.md
+++ README.md
@@ -9,8 +9,10 @@ Assumed expectation:
 The project seems to be focused on using webpack and npm. As well as practicing DOM manipulation by dynamincally rendering a restaurant homepage. 
 
 ------------ 
-Getting started:
+Getting started
 
 1. Initialized the git repository, git clone
 2. Initialize all webpack installs: 
 3. Setting up the HTML page
+4. Populate div#content
+    - hardcode html content via JS
--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 console.log("Hello from Webpack!");
 
+// grab the content div
+const content = document.getElementById("content");
 
+//create homepage elements
+const headline = document.createElement("h2");
+headline.textContent = "Welcome the Webpack Restaurant!";
+
+const descpription = document.createElement("p");
+descpription.textContent = "We server delicious food and provide a cozy atmosphere.";
+
+//append the content div
+content.appendChild(headline);
+content.appendChild(descpription);
